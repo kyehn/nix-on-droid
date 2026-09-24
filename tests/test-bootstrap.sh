f7bfe0570829e61d16d722dc9fd460a623e57f8c
kyehn <kyehn@users.noreply.github.com>
2026-09-24 14:49:30 +0000

    tests: 沙盒nix.conf补齐远程缓存substituters与公钥

--- tests/test-bootstrap.sh
+++ tests/test-bootstrap.sh
@@ -69,6 +69,8 @@ build-users-group =
 sandbox = false
 allow-unsafe-native-code-during-evaluation = true
 experimental-features = nix-command flakes
+substituters = https://cache.nixos.org https://seilunako.cachix.org https://nix-community.cachix.org
+trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= seilunako.cachix.org-1:e/aJJI1S5hPY/BPeiVZcuPjt5ZjBRRo9dlYHmvwXPFM= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=
 max-jobs = 1
 cores = 1
 EOF
